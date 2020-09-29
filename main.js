4d789cc5ce0cf0af66dd2caff0dab41016ae1803
SomeRandomDolphin <38103304+SomeRandomDolphin@users.noreply.github.com>
2020-09-29 19:57:18 +0700

    Update main.js

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ client.on("ready", () => {
 })
 
 client.on("guildCreate", guild => {
-    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-});
+    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`)
+})
   
 client.on("guildDelete", guild => {
-    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-});
+    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`)
+})
 
 client.on('presenceUpdate', async (oldPresence, newPresence) => {
     let member = newPresence.member
@@ -31,36 +31,37 @@ client.on('presenceUpdate', async (oldPresence, newPresence) => {
                 footer: { text: "Requested by " + botinfo.tag, 
                 icon_url: (botinfo.avatarURL()) },
                 timestamp: new Date()
-            };
+            }
             const offline = {
                 title: "🔪  " + botinfo.username + " is now offline!",
                 description : 'Bot e matek anjer :v',
                 footer: { text: "Requested by " + botinfo.tag, 
                 icon_url: (botinfo.avatarURL()) },
                 timestamp: new Date()
-            };
+            }
             const connection = {
                 title: "📶  " + botinfo.username + " had a connection problem!",
                 description : 'Indihomo paling mabok lagi',
                 footer: { text: "Requested by " + botinfo.tag, 
                 icon_url: (botinfo.avatarURL()) },
                 timestamp: new Date()
-            };
+            }
 
             //console.log(newPresence.activities[0])
 
-            if (newPresence.activities[0] === undefined) {
+            if (newPresence.status === "offline") {
+                user.send({ embed: offline })
+            }
+            else if (newPresence.activities[0] === undefined) {
                 user.send({ embed: connection })
             }
             else if (newPresence.status === "online") {
                 user.send({ embed: online })
             } 
-            else if (newPresence.status === "offline") {
-                user.send({ embed: offline })
-            }
+            
         }
     }
-});
+})
 
 client.login(config.token)
 
